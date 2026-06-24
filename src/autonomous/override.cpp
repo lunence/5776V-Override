9f263b92c96acaa987e1e31aaec7e365fbbee718
glasseshead <glasseshead.tetra@gmail.com>
2026-06-24 16:17:50 -0700

    while the motors are cooling i updated the movements and now it drifts in a good way
    yaayaay!

--- src/autonomous/override.cpp
+++ src/autonomous/override.cpp
@@ -18,8 +18,8 @@ void override() {
 
     //go to the quart
     chassis.turnToHeading(-90, 1000);
-    chassis.moveToPoint(67.5, -30, 1500);
-    chassis.moveToPoint(40.5, -30, 1500);
+    chassis.moveToPoint(67.5, -30, 1500, {.minSpeed = 110, .earlyExitRange = 2});
+    chassis.moveToPoint(40.5, -30, 1500, {.maxSpeed = 80, .minSpeed = 40, .earlyExitRange = 2});
     
     // in the event it turns away from 90 consistently this is to adjust it
     chassis.turnToHeading(-90, 1000);
@@ -30,23 +30,11 @@ void override() {
     pros::delay(350);
 
     // go to the wall to distreset
-    chassis.moveDistance(12, 1000, {.minSpeed = 25, .earlyExitRange = 0.5});
-    chassis.moveToPoint(45, 20, 1500);
+    chassis.turnToHeading(-50, 1000);
+    chassis.moveDistance(50, 1000, {.minSpeed = 25, .earlyExitRange = 0.5});
+    chassis.moveToPoint(-10, 30, 1500);
     chassis.turnToHeading(0, 1000);
 
     // reset distance sensor 
     chassis.distanceReset('L', 'B');
-
-    // // get pin near wall
-    // chassis.turnToHeading(-21, 1000);
-    // chassis.moveDistance(24.675, 1000, {.minSpeed = 12.5, .earlyExitRange= 0.5});
-    // chassis.turnToHeading(27, 500);
-
-    // // align to diagonal
-    // chassis.moveDistance(27,1000, {.minSpeed = 12.5, .earlyExitRange= 0.5});
-    // chassis.turnToHeading(135, 1000, {.minSpeed = 50});
-
-    // // get first pin, then second
-    // chassis.moveDistance(28, 1000, {.minSpeed = 80, .earlyExitRange=2});
-    // chassis.moveDistance(30, 1000, {.minSpeed = 90});
 }
