8dc5c7e06cc32d4b3ce5d240c71c517fcf96783a
glasseshead <glasseshead.tetra@gmail.com>
2026-06-24 20:54:54 -0700

    mooncy please please i hope this is good enough to pass your senior approval

--- src/autonomous/override.cpp
+++ src/autonomous/override.cpp
@@ -9,17 +9,18 @@ void override() {
     chassis.distanceReset('R', 'F');
 
     // get the first pin
-    chassis.moveToPoint(94, -64, 750, {.minSpeed = 80, .earlyExitRange = 0.8});
+    chassis.moveToPoint(94, -64, 750, {.minSpeed = 80, .earlyExitRange = 0.4});
     chassis.moveToPoint(110, -54, 750);
 
     // turn to the next pin and get it
     chassis.turnToHeading(-45, 650);
-    chassis.moveToPoint(95, -30, 1500, {.minSpeed = 80});
+    chassis.moveToPoint(95, -32, 1500, {.minSpeed = 80});
 
     //go to the quart
-    chassis.turnToHeading(-90, 600);
-    chassis.moveToPoint(67.5, -30, 1000, {.minSpeed = 110, .earlyExitRange = 2});
-    chassis.moveToPoint(40.5, -30, 800, {.maxSpeed = 50, .minSpeed = 40});
+    chassis.turnToHeading(-87, 600);
+    chassis.moveDistance(27, 1000, {.minSpeed = 110, .earlyExitRange = 2});
+    chassis.moveDistance(20, 1000, {.maxSpeed = 50, .minSpeed = 40});
+    // chassis.moveToPoint(50, -32, 800, {.maxSpeed = 50, .minSpeed = 40});
     
     // in the event it turns away from 90 consistently this is to adjust it
     chassis.turnToHeading(-90, 1000);
@@ -30,28 +31,34 @@ void override() {
     pros::delay(350);
 
     // go to the wall to distreset
-    chassis.turnToHeading(-50, 800);
-    chassis.moveDistance(48, 1000, {.minSpeed = 75, .earlyExitRange = 0.5});
+    chassis.turnToHeading(-60, 800);
+    chassis.moveDistance(48, 1000, {.minSpeed = 90, .earlyExitRange = 0.5});
+
+    // turn to a bit tilted to get more clearance
     chassis.turnToHeading(5, 600);
 
     // reset distance sensor 
-    // chassis.distanceReset('L', 'B');
+    chassis.distanceReset('L', 'B');
 
     // move to pin
     chassis.moveDistance(24.5, 1000);
     chassis.turnToHeading(-90, 600);
+
+    // get le pin because france and align to the line based on the grid
     chassis.moveDistance(5, 300, {.minSpeed = 80});
-    chassis.moveDistance(-5, 300, {.forwards = false});
+    chassis.moveDistance(-32, 300, {.forwards = false, .minSpeed = 80});
+
+    // turn back to forwards
     chassis.turnToHeading(0, 600);
 
     // align to line
-    chassis.moveToPoint(10, 45, 800);
-    chassis.turnToHeading(133, 850);
+    chassis.turnToHeading(133, 850, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE});
 
-    // go to the center
-    chassis.moveDistance(36, 1500, {.maxSpeed = 80, .earlyExitRange = 2});
+    // go to the center 
+    chassis.moveDistance(36, 1500, {.maxSpeed = 90, .minSpeed = 80, .earlyExitRange = 2});
 
-    chassis.turnToHeading(133, 850);
+    // previously used to make sure robot was fully aligned
+    // chassis.turnToHeading(133, 850, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE, .earlyExitRange = 1});
 
-    chassis.moveDistance(30, 1500, {.minSpeed = 100});
+    chassis.moveDistance(15, 1500, {.minSpeed = 110});
 }
