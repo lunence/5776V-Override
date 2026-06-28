b327973593b719508bb7e5f9c54627f015f6f9f4
glasseshead <glasseshead.tetra@gmail.com>
2026-06-27 18:56:05 -0700

    i do hope drivecode is done now to wait for my buildy friends to give us proggy kids some ports

--- src/drivecode/util.cpp
+++ src/drivecode/util.cpp
@@ -22,19 +22,27 @@ void sensorInit() {}
 //begin all tasks
 void taskInit() {
     pros::Task screenTask(runScreen, "screen task");
-    // pros::Task sdWriteTask(sdWriter::runWriter, "sd write task");
     pros::Task cascadeTask(runCascade, "cascade task");
     pros::Task intakeTask(runIntake, "intake task");
     pros::Task pistonTask(runPistons, "pistons task");
+    
+    // pros::Task sdWriteTask(sdWriter::runWriter, "sd write task");
 }
 
 //brain task
 void runScreen() {
     while(true) {
-        pros::screen::print(pros::E_TEXT_MEDIUM, 1, "Cascade State: %d", cascadeState);
-        pros::screen::print(pros::E_TEXT_MEDIUM, 2, "DistSensor: %.3f", distCascade.get());
+        lemlib::Pose pose = chassis.getPose();
 
-        pros::delay(10);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 1, "X: %.3f", pose.x);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 2, "Y: %.3f", pose.y);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Theta: %.3f", pose.theta);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 4, "Front: %.2f in", distFrontLeft.get() / 25.4);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 5, "Right: %.2f in", distRight.get() / 25.4);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 6, "Left:  %.2f in", distLeft.get() / 25.4);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 7, "Back:  %.2f in", distBack.get() / 25.4);
+
+        pros::delay(500);
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@ void competition_initialize() {}
 
 void autonomous() {
 	chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
+	override();
 } 
 
 void opcontrol() {
